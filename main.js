e3d6ab9b5bfc485e452edc7d99c069719903b8a3
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-05-14 15:03:15 +0200

    logo

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// Navbar
 let btnMenu = document.getElementById('menu');
 let btnClose = document.getElementById('close');
 let container = document.querySelector('.container');
@@ -12,4 +14,32 @@ btnClose.onclick = function () {
                     this.classList.add('hide');
                     btnClose.classList.remove('hide');
                     container.classList.remove('hide');
-                }
+}
+                
+// Zoekbalk
+
+   let searchInput = document.getElementById("searchbar_got");
+   searchInput.onkeyup = searchGOT;
+
+   function searchGOT() {
+       const table = document.getElementById(‘gottable’);
+       const allCharacters = table.getElementsByTagName(’tr’);
+   }
+
+   for (var i=1; i < allCharacters.length; i++) {
+       var cellData = allCharacters[i].getElementsByTagName(’td’);
+       for (var j=0; j < cellData.length; j++) {
+          if(cellData[j].innerHTML.toUpperCase().indexOf(searchValue) > -1) {
+          var hit = true;
+       }
+   }
+
+   if (hit) {
+      allCharacters[i].style.display = "";
+      hit = false;
+   } else {
+      if (i > 0) {
+        allCharacters[i].style.display = "none";
+      }  
+   } }
+
